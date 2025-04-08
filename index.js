64cd8c6a3f0b09711eaed789566225cf9f945ffb
Jacob Phillips <jsp452@msstate.edu>
2025-04-08 09:43:27 -0500

    Add .gitignore

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-const {Client} = require('pg');
+var mysql = require('mysql2');
+const express = require('express');
+const app = express();
 
-const connection = new Client({
-    host:'localhost',
-    port:5432,
-    user:'postgres',
-    password:'admin',
-    database: 'myDB'
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-connection.connect().then(() => console.log("Connected"))
+app.get('/', (req, res) => {
+    res.render('test.ejs');
+});
+
+
+// users CRUD -----------------------------------------------------------------------------------------------
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM User', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+
+app.post('/users', (req, res) => {
+    const user = req.body;
+    var sql = "INSERT INTO User (Email, Password, Classification) VALUES ('" + user.Email + "', '" + user.Password + "', '" + user.Classification + "')";
+    connection.query(sql, user, (err, results) => {
+        if (err) throw err;
+        res.json({ id: results.insertId, ...user });
+    });
+});
+
+
+app.put('/users/:id', (req, res) => {
+    const user = req.body;
+    connection.query('UPDATE User SET ? WHERE id = ?', [user, req.params.id], (err, results) => {
+        if (err) throw err;
+        res.json({ id: req.params.id, ...user });
+    });
+});
+
+
+app.delete('/users/:id', (req, res) => {
+    connection.query('DELETE FROM users WHERE id = ?', req.params.id, (err, results) => {
+        if (err) throw err;
+        res.json({ id: req.params.id });
+    });
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+var connection = mysql.createConnection({
+    host: 'db4free.net',
+    user: 'asdlfkj12345',
+    password: 'drghudla',
+    database: 'asdlfkj12345'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
+});
+
